1f1db11fe824c7ed3ed8755a05f54eccd1211b33
Jose Arias <developstuff93@gmail.com>
2021-06-04 00:18:14 -0400

    add useCheckoutMetadata for metadata update

---
+++ src/@next/CheckoutUtils/constants.tsx
@@ -0,0 +1,22 @@
+export const CheckoutMetadataTypes = {
+  Title: "vacancy_jobTitle",
+  Description: "vacancy_description",
+  Industry: "vacancy_taxonomy_industry",
+  Category: "vacancy_taxonomy_jobCategoryId",
+  Seniority: "vacancy_taxonomy_seniority",
+  // Seniority: 'vacancy_taxonomy_seniorityId'
+  VacancyURL: "vacancy_tracking_vacancy_url",
+  ApplicationURL: "vacancy_tracking_applicationUrl",
+  ContactName: "contactInfo_name",
+  ContactNumber: "contactInfo_phoneNumber",
+  // vacancy_organizationName = serializers.CharField(max_length=255, allow_null=False)
+  VacancyType: "vacancy_type",
+  MinExp: "vacancy_minimumYearsOfExperience",
+  EducationLevel: "vacancy_educationLevelId",
+  MinWorkingHours: "vacancy_workingHours_minimum",
+  MaxWorkingHours: "vacancy_workingHours_maximum",
+  SalaryMinAmount: "vacancy_salary_minimumAmount",
+  SalaryMaxAmount: "vacancy_salary_maximumAmount",
+  SalaryPerPeriod: "vacancy_salary_perPeriod",
+  SalaryCurrency: "vacancy_salary_currency",
+};
---
+++ src/@next/hooks/useCheckoutMetadata.tsx
@@ -0,0 +1,43 @@
+/* eslint-disable no-console */
+
+import { useEffect, useState } from "react";
+
+import { useLocalStorage } from "./useLocalStorage";
+
+export const useCheckoutMetadata = () => {
+  const {
+    storedValue: checkoutData,
+    setValue: setCheckoutData,
+  } = useLocalStorage("data_checkout");
+
+  const [metadata, setStoredMetadata] = useState(checkoutData?.metadata);
+
+  useEffect(() => {
+    setCheckoutData({
+      ...checkoutData,
+      metadata,
+    });
+  }, [metadata]);
+
+  const setMetadata = (value: any) => {
+    setStoredMetadata(value);
+  };
+
+  const appendMetadata = (appendData: any) => {
+    setMetadata({
+      ...metadata,
+      ...appendData,
+    });
+  };
+
+  const setMetadataField = (field: any, value: any) => {
+    if (!field || !value) {
+      return;
+    }
+    appendMetadata({
+      [field]: value,
+    });
+  };
+
+  return { metadata, setMetadata, appendMetadata, setMetadataField };
+};
