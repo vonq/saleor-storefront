92c0e0a555bfc5cf515363f6fb8df55672a1f0fd
mingthegit <ming.dev1026@gmail.com>
2021-07-06 12:39:57 +0800

    integrated material ui library

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,28 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
+import { ServerStyleSheets } from "@material-ui/core/styles";
 import React from "react";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
+    // Render app and page and get the context of the page with collected side effects.
+    const sheets = new ServerStyleSheets();
+    const originalRenderPage = ctx.renderPage;
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: App => props => sheets.collect(<App {...props} />),
+      });
+
     const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+
+    return {
+      ...initialProps,
+      // Styles fragment is rendered after the app and page rendering finish.
+      styles: [
+        ...React.Children.toArray(initialProps.styles),
+        sheets.getStyleElement(),
+      ],
+    };
   }
 
   render() {
