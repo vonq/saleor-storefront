f620c83176354e24657b724ccf64827e13b91ccd
Jose Arias <developstuff93@gmail.com>
2021-07-21 22:44:30 -0400

    add JobAdSubTitleHeader component for the form fields

---
+++ src/@next/pages/CreateJobAd/JobAdSubTitleHeader.tsx
@@ -0,0 +1,24 @@
+import { Box, Typography } from "@material-ui/core";
+import React, { ReactNode } from "react";
+
+interface IProps {
+  title: string;
+  description?: string;
+  children: ReactNode;
+}
+
+const JobAdSubTitleHeader = ({ title, description, children }: IProps) => (
+  <Box display="flex" flexDirection="column">
+    <Typography variant="h6" component="h3">
+      {title}
+    </Typography>
+    {description && (
+      <Typography variant="body2" component="p">
+        {description}
+      </Typography>
+    )}
+    {children}
+  </Box>
+);
+
+export default JobAdSubTitleHeader;
