edf56cd993920169169fdabc62faae141b7e0f98
mingthegit <ming.dev1026@gmail.com>
2021-07-26 16:09:29 +0800

    fixed wrong merge conflicts fix

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,5 @@
 export const apiUrl = process.env.NEXT_PUBLIC_API_URI;
 export const pkbUrl = process.env.NEXT_PUBLIC_PKB_URI;
-export const pkbAuth = process.env.NEXT_PUBLIC_PKB_AUTH;
 export const vacancyUrl = process.env.NEXT_PUBLIC_VACANCY_URI;
 export const sentryDsn = process.env.NEXT_PUBLIC_SENTRY_DSN;
 const sampleRate = parseFloat(process.env.NEXT_PUBLIC_SENTRY_APM || "");
@@ -11,5 +10,5 @@ export const demoMode = process.env.NEXT_PUBLIC_DEMO_MODE === "true";
 export const ssrMode = typeof window === "undefined";
 export const channelSlug = process.env.NEXT_PUBLIC_SALEOR_CHANNEL_SLUG;
 
-// @FIXME: pull from user metadata
+// @FIXME: should be pulled from Auth0 user metadata later
 export const companyId = "c5c6e9e5-ded0-4b41-9214-a874c5a6d7fb";
